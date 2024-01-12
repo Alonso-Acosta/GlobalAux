3b3825259b75cc1a9d0b10994f6aaa0f28571142
Alonso acosta <harinson8@gmail.com>
2024-01-12 13:01:05 -0500

    ProyectosYCatalogos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify/functions";
 import react from "@astrojs/react";
-
-import solidJs from "@astrojs/solid-js";
-
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
-  integrations: [react(), solidJs()]
+  integrations: [react()]
 });
